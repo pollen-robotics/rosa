f0a6eba8c5a2dcb95d089168d991dec077d6c895
Pierre Rouanet <pierre.rouanet@gmail.com>
2018-08-28 11:41:12 +0200

    Add serial to ws python utility.

---
+++ serial2ws.py
@@ -0,0 +1,72 @@
+import json
+import serial
+
+from threading import Thread
+
+from tornado.ioloop import IOLoop
+from tornado.web import Application
+from tornado.websocket import WebSocketHandler
+
+
+class SerialToWs(WebSocketHandler):
+    def open(self):
+        print('Connection opened.')
+        self.serial = serial.Serial(self.serial_port, baudrate=115200)
+        Thread(target=self._check_msg).start()
+
+    def on_close(self):
+        if self.verbose:
+            print('Connection closed.')
+        self.serial.close()
+
+    def on_message(self, message):
+        message += '\n'
+
+        if self.verbose:
+            print('WS->Ser: {}'.format(message))
+        self.serial.write(message.encode())
+
+    def send(self, message):
+        if self.verbose:
+            print('Ser->WS: {}'.format(message))
+        self.ioloop.add_callback(lambda: self.write_message(message))
+
+    def _check_msg(self):
+        while True:
+            r = self.serial.readline()
+            try:
+                json.loads(r)
+            except ValueError:
+                print('Dump {}'.format(r))
+                continue
+            self.send(r)
+
+    def check_origin(self, origin):
+        return True
+
+
+def main():
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--serial-port', type=str, required=True)
+    parser.add_argument('--ws-port', type=int, required=True)
+    parser.add_argument('--verbose', action='store_true', default=False)
+    args = parser.parse_args()
+
+    loop = IOLoop()
+
+    SerialToWs.serial_port = args.serial_port
+    SerialToWs.verbose = args.verbose
+    SerialToWs.ioloop = loop
+
+    app = Application([
+        (r'/', SerialToWs)
+    ])
+
+    app.listen(args.ws_port)
+    loop.start()
+
+
+if __name__ == '__main__':
+    main()
