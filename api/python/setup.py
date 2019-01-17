9c719bee0a5ea4d7a1501b99836d1c5d7bacd5ce
Pierre Rouanet <pierre.rouanet@gmail.com>
2019-01-17 11:15:05 +0100

    Setup python API project.

---
+++ api/python/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+import imp
+from setuptools import setup, find_packages
+
+version = imp.load_source('rosa.version', 'rosa/version.py')
+
+
+setup(name='rosa',
+      version=version.version,
+
+      description='Rosa robot Python API',
+      author='Pollen Robotics',
+      url='https://github.com/pollen-robotics/rosa',
+
+      packages=find_packages(),
+      )
