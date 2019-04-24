105117148e4e6d741485e7c4ff5ff6363255666d
Pierre Rouanet <pierre.rouanet@gmail.com>
2019-04-24 12:04:08 +0200

    Add impl. for ground/proximity/color sensor and buzz/led.

--- rpi/io_controller.py
+++ rpi/io_controller.py
@@ -1,7 +1,9 @@
+import time
+import smbus
 import numpy as np
 import gpiozero as gpio
 
-pins = {}
+from apds9960 import APDS9960
 
 
 ###########################
@@ -52,24 +54,92 @@ def motor_short_brake(motor):
     in2.on()
 
 
-def buzz(duration):
-    # TODO: impl.
-    print('BUUUUUUUUZZZZZ for', duration, 's')
-    # pins['buzzer'].beep(on_time=duration, n=1)
-    pass
+#################################
+# I2C Proximity/Color functions #
+#################################
+
+i2c_channels = {
+    'GU2': 0x01,
+    'GU3': 0x02,
+    'GU4': 0x04,
+    'GU5': 0x08,
+    'FU1': 0x10,
+    'FU2': 0x20,
+    'FU4': 0x40,
+}
+
+mux_address = 0x70
+i2c_bus = smbus.SMBus(1)
+ground_threshold = 200
+
+
+def set_i2c_channel(channel):
+    if channel not in i2c_channels.keys():
+        raise ValueError('channel should be one of {}'.format(list(i2c_channels.keys())))
+
+    # 0x04 is the register for switching channels
+    # See http://www.ti.com/lit/ds/symlink/tca9548a.pdf
+    i2c_bus.write_byte_data(mux_address, 0x04, i2c_channels[channel])
 
 
 def get_ground(sensor):
-    # TODO: impl.
-    # return pins['ground-{}'.format(sensor)].value()
-    return np.random.rand()
+    return get_dist(sensor) > ground_threshold
 
 
 def get_color(sensor):
-    # TODO: impl.
-    return (np.random.rand(), np.random.rand(), np.random.rand())
+    set_i2c_channel(sensor)
+
+    apds = APDS9960(i2c_bus)
+    apds.enableLightSensor()
+
+    time.sleep(0.11)
+
+    red = apds.readRedLight()
+    green = apds.readGreenLight()
+    blue = apds.readBlueLight()
+    ambient = apds.readAmbientLight()
+
+    return (red, green, blue, ambient)
 
 
 def get_dist(sensor):
-    # TODO: impl.
-    return np.random.rand()
+    set_i2c_channel(sensor)
+
+    apds = APDS9960(i2c_bus)
+    apds.enableProximitySensor()
+
+    time.sleep(0.01)
+
+    return apds.readProximity()
+
+
+##################
+# Buzzer utility #
+##################
+
+buzzer = gpio.Buzzer(13, active_high=False)
+
+
+def buzz(duration):
+    buzzer.beep(on_time=duration, n=1)
+
+
+################
+# Leds utility #
+################
+
+leds = [gpio.LED(5), gpio.LED(6)]
+
+
+def led_on(led_id):
+    if not (1 <= led_id <= len(leds)):
+        raise ValueError('led_id should be in (1, {})!'.format(len(leds)))
+
+    leds[led_id - 1].on()
+
+
+def led_off(led_id):
+    if not (1 <= led_id <= len(leds)):
+        raise ValueError('led_id should be in (1, {})!'.format(len(leds)))
+
+    leds[led_id - 1].off()
