af40c113f2521558cbec0c14b89d1fc5cb4e1fc3
Pierre Rouanet <pierre.rouanet@gmail.com>
2019-04-25 11:53:18 +0200

    Push state in bg for Python APIs to avoid buff. overflow on client side.

--- api/python/rosa/remote_io.py
+++ api/python/rosa/remote_io.py
@@ -1,7 +1,19 @@
 import json
+import time
+import collections
 import websocket as ws
 
-from threading import Thread, Event
+from threading import Thread, Event, Lock
+
+
+# See https://stackoverflow.com/questions/3232943/update-value-of-a-nested-dictionary-of-varying-depth
+def update_cmd(d, u):
+    for k, v in u.items():
+        if isinstance(v, collections.Mapping):
+            d[k] = update_cmd(d.get(k, {}), v)
+        else:
+            d[k] = v
+    return d
 
 
 class RemoteIO(object):
@@ -15,25 +27,46 @@ class RemoteIO(object):
         self._poll_t.daemon = True
         self._poll_t.start()
 
+        self._cmd = {}
+        self._cmd_lock = Lock()
+        self._cmd_event = Event()
+        self._push_t = Thread(target=self._push_cmd)
+        self._push_t.daemon = True
+        self._push_t.start()
+
         self._synced = Event()
         self._synced.wait()
 
+    def push_cmd(self, cmd):
+        with self._cmd_lock:
+            update_cmd(self._cmd, cmd)
+            self._cmd_event.set()
+
+    def _push_cmd(self):
+        while True:
+            self._cmd_event.wait()
+            with self._cmd_lock:
+                self.ws.send(json.dumps(self._cmd))
+                self._cmd.clear()
+                self._cmd_event.clear()
+            time.sleep(1 / 50)
+
     def set_speed(self, motor, speed):
-        self.ws.send(json.dumps({
+        self.push_cmd({
             'wheels': {
                 motor: speed
             }
-        }))
+        })
 
     def set_led(self, led, val):
-        self.ws.send(json.dumps({
+        self.push_cmd({
             'leds': {
                 led: val
             }
-        }))
+        })
 
     def buzz(self, duration):
-        self.ws.send(json.dumps({'buzz': duration}))
+        self.push_cmd({'buzz': duration})
 
     def _update_state(self):
         while True:
